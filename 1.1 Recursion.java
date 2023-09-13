a0545897e03246b508c0b5c84a9057ab74ffd4b1
bamee1234 <125195060+bamee1234@users.noreply.github.com>
2023-09-13 12:57:41 +0700

    Lab1

--- 1.1 Recursion.java
+++ 1.1 Recursion.java
@@ -1,5 +1,5 @@
 public class Recursion {
-/*
+	
 	static int subsum(int n) {
 		//Ex1. Complete the content of this method 
         if (n == 1) {
@@ -9,7 +9,6 @@ public class Recursion {
           return (int) ((Math.pow(-1, n - 1) * n) + subsum (n - 1));
         }
     }
-*/
 	static int sumDigit(int n) {
 		//Ex2. Complete the content of this method 
         if (n < 10) {
@@ -20,24 +19,17 @@ public class Recursion {
      }
         
     }
-    
+	
 	public static void main(String[] args) {
-          
 
-            // Test Uncomment these lines below to test your subsum code
-/*
-               System.out.println("Calculating subsum(10):");
-		     System.out.println("Your answer is "+subsum(10));
-               System.out.println("The correct answer is -5");
-               System.out.println("-----------------------"); 
-*/
+		System.out.println("Calculating subsum(10):");
+		System.out.println("Your answer is "+subsum(10));
+		System.out.println("The correct answer is -5");
+		System.out.println("-----------------------"); 
               
-
-            // Uncomment these lines below to test your sumDigit code
-            
-               System.out.println("sumDigit(123456789)");
-		     System.out.println("Your answer is "+ sumDigit(123456789));
-               System.out.println("The correct answer is 45");
+		System.out.println("sumDigit(123456789)");
+		System.out.println("Your answer is "+ sumDigit(123456789));
+		System.out.println("The correct answer is 45");
             
 	}
     
